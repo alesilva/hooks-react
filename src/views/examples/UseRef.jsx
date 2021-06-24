69bf9f102b4f125e5f75bc62699e30fb3c1e7c5e
Alexandre Ribeiro da Silva <alribeiro@uolinc.com>
2021-06-24 19:26:03 -0300

    Aula UseRef

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -1,13 +1,62 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
+const merge = function(s1, s2) {
+    let max = s1.length > s2.length ? s1.length : s2.length;
+    let result = ''
+    for (let i = 0; i < max; i++) {
+        result += ((s1[i] || '') + (s2[i] || ''))
+    }
+    return result;
+}
 
 const UseRef = (props) => {
+    const [value1, setValue1] = useState("")
+    const [value2, setValue2] = useState("")
+    
+    const count = useRef(0)
+    const myInput1 = useRef(null)
+    const myInput2 = useRef(null)
+
+    useEffect(function() {
+        count.current++
+        myInput2.current.focus()
+    }, [value1])
+
+    useEffect(function() {
+        count.current++
+        myInput1.current.focus()
+    }, [value2])
+
     return (
         <div className="UseRef">
             <PageTitle
                 title="Hook UseRef"
                 subtitle="Retorna um objeto mutável com a propriedade .current!"
             />
+
+            <SectionTitle title="Exercicio #01" />
+            <div className="center">
+                <div>
+                    <span className="text">Valor: </span>
+                    <span className="text">{merge(value1, value2)} [</span>
+                    <span className="text red">{count.current}</span>
+                    <span className="text">]</span>
+                </div>
+                <input type="text" className="text" 
+                    ref={myInput1}
+                    value={value1} 
+                    onChange={e => setValue1(e.target.value)}/>
+            </div>
+
+            <SectionTitle title="Exercicio #02" />
+            <div className="center">
+                <input type="text" className="input" 
+                    ref={myInput2}
+                    value={value2}
+                    onChange={e => setValue2(e.target.value)}/>
+            </div>
         </div>
     )
 }
